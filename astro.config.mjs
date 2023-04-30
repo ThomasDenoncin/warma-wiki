e297ade88f86e866eafd3fa3db6d39a66856469a
ThomasDenoncin <thomas.denoncin@gmail.com>
2023-04-30 22:30:40 +0200

    add formulaire

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
-})
+  integrations: [mdx(), alpinejs()]
+});
